c76d29f28e69470c474d9fe728ae64671d2ce378
Chris Johnson <johnsonc.88@gmail.com>
2016-10-15 17:25:08 -0700

    add an animation to display a list of games

---
+++ simple-animation/animation.js
@@ -0,0 +1,47 @@
+d3.select('#animation-div').selectAll('svg')
+  .data([null])
+  .enter()
+  .append('svg')
+  .style('width', '800px')
+  .style('height', '600px')
+  .style('background-color', 'blue');
+
+var svg = d3.select("svg");
+
+
+function makeCircle() {
+  var circle = svg.selectAll("circle")
+    .data(['flappy bird', 'frogger', 'potato', 'thumb', 'othergame', "more game"]);
+  
+  circle.enter().append("circle")
+    .attr("cy", 100)
+    .attr("cx", 0)
+    .attr("r", 18)
+    .attr("class", function(d){ return d });
+};
+
+makeCircle();
+
+function moveCircleRight() {
+  svg.selectAll('circle')
+    .transition()
+    .delay( function(d, i){ return 150 * i } )
+    .duration(1500)
+    .attr('cx', function(d, i){ return 700 - (150 * i) });
+};
+
+moveCircleRight()
+
+function moveCircleDown() {
+  svg.selectAll('circle')
+    .transition()
+    .delay( function(d, i){ return 50 * i } )
+    .duration(1000)
+    .attr('cy', 500);
+};
+
+setTimeout(moveCircleDown, 1500);
+
+function displayTitle() {
+
+};
